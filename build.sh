0256d0efeb2d7dd6e217d529a15532ef3a2e24a3
ErrorFYM <errorfymdev@gmail.com>
2024-10-30 12:48:21 +0200

    Improved filename selection

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 if [ ! -d "venv_linux" ]; then ./create_venv.sh
 fi
 
-./venv_linux/bin/pyinstaller -F --add-data archive/$(./venv_linux/bin/python3 -c "from config import archive_name; print(archive_name)"):archive -n $(./venv_linux/bin/python3 -c "from config import archive_name; print(archive_name.split('.')[0])") main.py
+./venv_linux/bin/pyinstaller -F --add-data archive/$(./venv_linux/bin/python3 -c "from config import archive_name; print(archive_name)"):archive -n $(./venv_linux/bin/python3 -c "from config import archive_name; print(archive_name.split('.tar')[0])") main.py
 rm -r build
-rm $(./venv_linux/bin/python3 -c "from config import archive_name; print(archive_name.split('.')[0])").spec
+rm $(./venv_linux/bin/python3 -c "from config import archive_name; print(archive_name.split('.tar')[0])").spec
