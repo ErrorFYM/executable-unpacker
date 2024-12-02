f0259c58c1fc19a61e827aa0762f4adc13a7580f
ErrorFYM <errorfymdev@gmail.com>
2024-12-02 23:00:45 +0200

    Added destination folder existance indicator

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 from os.path import expanduser
+from os.path import isdir as path_isDir
 
 archive_name = "archive.tar.xz"  # must be tar format, any method could be used. symbolic links could be used
 
 default_unpack_path = expanduser("~")  # user folder
 can_change_unpack_path = True  # will client be able to change installation path in compilated file (if False, it will instantly extract all the files in default_unpack_path)
+show_if_unpack_path_exists = True  # will show "(exists)" or "(doesn't exist)" at the end of msg_path_input
 
 colored_terminal = True
 restart_colors = "\x1b[0m"  # do not change
@@ -12,8 +14,24 @@ unpack_path = default_unpack_path
 
 # Messages
 def set_messages():  # setting messages with up to date information (will be called in main.py when ready)
-    global msg_path_input, msg_extracting_to, msg_extracted_successfully
-    msg_path_input = f"Enter target path(leave empty for {default_unpack_path} ): "
+    global msg_path_exists, msg_path_input, msg_extracting_to, msg_extracted_successfully
+    
+    if show_if_unpack_path_exists:
+        if path_isDir(default_unpack_path):
+            if colored_terminal:      # COLOR CHECK FOR PATH EXISTS MESSAGE
+                msg_path_exists = "\x1b[0;32;40m(exists)" + restart_colors  # green color
+            else:
+                msg_path_exists = "(exists)"
+        else:
+            if colored_terminal:
+                msg_path_exists = "\x1b[0;31;40m(doesn't exist)" + restart_colors  # reg color
+            else:
+                msg_path_exists = "(doesn't exist)"
+
+    else:
+        msg_path_exists = ""
+    
+    msg_path_input = f"Enter target path(leave empty for {default_unpack_path} {msg_path_exists}): "
 
     global msg_extracting_to_pure, msg_extracting_to_color
     msg_extracting_to_pure = f"Extracting to {unpack_path}…"
